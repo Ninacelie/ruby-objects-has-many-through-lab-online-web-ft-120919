522dcbd5cc7cb0ef7cebe3cd9624d042f7f35135
Nina Angelo <ninacelie@gmail.com>
2020-01-08 20:36:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,9 @@
-class Artist 
+class Artist
 
 
 
-  
-end 
+
+end
 
 
 
@@ -15,36 +15,36 @@ end
 
 
 # class Artist
-# 
+#
 #   attr_accessor :name
-# 
+#
 #   @@all = []
-# 
+#
 #     def initialize(name)
 #       @name = name
 #       @@all << self
 #     end
-# 
+#
 #     def self.all
 #       @@all
 #     end
-# 
+#
 #     def new_song(name, genre)
 #       songs = Song.new(name, self, genre)
 #       self.songs.last
 #     end
-# 
+#
 #     def songs
 #       Song.all.select do |song|
 #         song.artist == self
 #       end
 #     end
-# 
+#
 #     def genres
 #       Song.all.map do |song|
 #         song.genre
 #       end
 #     end
-# 
-# 
+#
+#
 # end
